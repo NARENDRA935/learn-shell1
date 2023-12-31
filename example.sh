d7c13b7024e5798a0ff36ce4c5a7c6a0c20cd82a
Narendra T <narendratarra935@gmail.com>
2023-12-31 18:10:41 +0530

    addition

--- example.sh
+++ example.sh
@@ -1,10 +1,11 @@
+
 length() {
-  num=$1
-  len=0
-  while[ $num -gt 0 ]
-  do
-    num =$((num/10))
-    len=$((len+1))
-  done
-  echo "$len"
+    num=$1
+    len=0
+    while [ $num -gt 0 ]
+    do
+        num=$((num/10))
+        len=$((len+1))
+    done
+    echo $len
 }
