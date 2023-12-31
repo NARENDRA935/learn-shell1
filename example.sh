54f4e5756463fcfd2b90ce20d6713794d084b3ba
Narendra T <narendratarra935@gmail.com>
2023-12-31 19:18:03 +0530

    addition

--- example.sh
+++ example.sh
@@ -1,5 +1,5 @@
 length() {
-    read num
+    num =$1
     len=0
     while [ $num -gt 0 ]
     do
@@ -8,5 +8,24 @@ length() {
     done
     echo $len
 }
-length $num
+armstrong(){
+  num=$1
+  sum=0
+  len=$(length $num)
+  temp=$num
+  while[$num -gt 0]
+  do
+    digit=$((temp % 10))
+    sum=$((sum+digit**len))
+    temp=$((temp/10))
+  done
+  if [$sum   -eq $num]
+  then
+    echo "$num is a armstrong"
+  else
+    echo "$num is not armstrong"
+}
+echo "enter the number"
+read num
+armstrong $num
 
