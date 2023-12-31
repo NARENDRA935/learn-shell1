ffad0f0ce09a7e0a09090f3dbdec0a9bcb0a0df6
Narendra T <narendratarra935@gmail.com>
2023-12-31 17:57:22 +0530

    addition

--- example.sh
+++ example.sh
@@ -1,8 +1,8 @@
-num=$1
-fact=1
-for((i=1; i<=num; i++))
-do
-        let fact=fact*i
-done
-
-echo "Factorial is $fact"
+length(){
+  num =$1
+  len=0
+  while[$num -gt 0]
+  num =$((num/10))
+  len=$((len+1))
+}
+echo "$len"
