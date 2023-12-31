2b64ae09bcad2be05cb8a9e5e4481f71e06cba28
Narendra T <narendratarra935@gmail.com>
2023-12-31 18:52:12 +0530

    addition

--- example.sh
+++ example.sh
@@ -1,5 +1,5 @@
 length() {
-    num=$1
+    read num
     len=0
     while [ $num -gt 0 ]
     do
@@ -8,33 +8,5 @@ length() {
     done
     echo $len
 }
-
-# Function to check if a number is Armstrong number
-check_armstrong() {
-    num=$1
-    sum=0
-    len=$(length $num)
-    temp=$num
-    while [ $temp -gt 0 ]
-    do
-        digit=$((temp % 10))
-        sum=$((sum + digit**len))
-        temp=$((temp/10))
-    done
-
-    if [ $sum -eq $num ]
-    then
-        echo "$num is an Armstrong number."
-    else
-        echo "$num is not an Armstrong number."
-    fi
-}
-
-# Read number
-echo "Enter a number: "
-read num
-
-# Call function
-check_armstrong $num
 
 
